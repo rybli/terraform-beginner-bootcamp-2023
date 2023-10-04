e2d726b84d1c2c222013278c2cf04ce4f194786f
rybli <ryanblitch93@gmail.com>
2023-10-04 23:53:56 +0000

    #25 refactor s3 bucket into new terrahouse_aws module

--- providers.tf
+++ providers.tf
@@ -1,23 +1,2 @@
-terraform {
-#   cloud {
-#     organization = "rybli-terraform-beginner-bootcamp-2023"
 
-#     workspaces {
-#       name = "terra-house-1"
-#     }
-#   }
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.18.1"
-    }
-  }
-}
 
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-}
