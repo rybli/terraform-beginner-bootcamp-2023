22fdef1aba86bbf363a80c1d187ef38359dd8986
rybli <ryanblitch93@gmail.com>
2023-10-05 01:51:13 +0000

    #27 enables static website hosting, uploaded index.html and error.html via terraform

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -15,6 +15,7 @@ resource "aws_s3_bucket" "website_bucket" {
   }
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
 resource "aws_s3_bucket_website_configuration" "website_configuration" {
   bucket = aws_s3_bucket.website_bucket.bucket
 
@@ -25,4 +26,19 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
   error_document {
     key = "error.html"
   }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+resource "aws_s3_object" "website_index" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key = "index.html"
+  source = "${path.root}/public/index.html"
+  etag = filemd5(var.index_html_filepath)
+}
+
+resource "aws_s3_object" "website_error" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key = "error.html"
+  source = "${path.root}/public/error.html"
+  etag = filemd5(var.error_html_filepath)
 }
